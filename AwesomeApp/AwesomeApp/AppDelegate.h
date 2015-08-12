e1e52af019bd8d55a53b587e580fff65701cd95c
Waqas Quershi <waqas.qureshi@wemotech.com>
2015-08-12 20:05:08 +0500

    added project

---
+++ AwesomeApp/AwesomeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AwesomeApp
+//
+//  Created by Waqas Quershi on 11/08/2015.
+//  Copyright (c) 2015 iGensol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AwesomeApp/AwesomeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AwesomeApp
+//
+//  Created by Waqas Quershi on 11/08/2015.
+//  Copyright (c) 2015 iGensol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
